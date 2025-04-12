98a4277d4c5a0627b82a16ddda87a05be8134dfc
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-04-12 19:33:58 +0400

    buttons are add

---
+++ app.js
@@ -0,0 +1,108 @@
+const plane = document.getElementById('plane');
+
+const LEFT_INPUT = document.querySelector(".l-input");
+const LEFT_PLUS_BUTTON = document.getElementById("l-plus");
+const LEFT_MINUS_BUTTON = document.getElementById("l-minus");
+const LEFT_BET_BUTTON = document.getElementById("l-bet-btn");
+
+const RIGTH_INPUT = document.querySelector(".r-input");
+const RIGTH_PLUS_BUTTON = document.getElementById("r-plus");
+const RIGTH_MINUS_BUTTON = document.getElementById("r-minus");
+const RIGTH_BET_BUTTON = document.getElementById("r-bet-btn");
+
+const LEFT_ONE_BUTTON = document.getElementById("l-one");
+const LEFT_FIVE_BUTTON = document.getElementById("l-five");
+const LEFT_TWENTYFIVE_BUTTON = document.getElementById("l-twenty-five");
+const LEFT_ONEHUNDRED_BUTTON = document.getElementById("l-one-hundred");
+
+const RIGTH_ONE_BUTTON = document.getElementById("r-one");
+const RIGTH_FIVE_BUTTON = document.getElementById("r-five");
+const RIGTH_TWENTYFIVE_BUTTON = document.getElementById("r-twenty-five");
+const RIGTH_ONEHUNDRED_BUTTON = document.getElementById("r-one-hundred");
+
+
+const airplane = () => {
+    
+};
+
+
+const betBtn = () => {
+  // LEFT SIDE BUTTONS
+
+  LEFT_PLUS_BUTTON.addEventListener("click", () => {
+    let value = parseFloat(LEFT_INPUT.value) || 1.0;
+    value += 1;
+    LEFT_INPUT.value = value.toFixed(2);
+    LEFT_BET_BUTTON.textContent = `${value.toFixed(2)} BET`;
+  });
+  LEFT_MINUS_BUTTON.addEventListener("click", () => {
+    let value = parseFloat(LEFT_INPUT.value) || 1.0;
+    if (value > 1) {
+      value -= 1;
+      LEFT_INPUT.value = value.toFixed(2);
+      LEFT_BET_BUTTON.textContent = `${value.toFixed(2)} BET`;
+    }
+  });
+
+  // RIGHT SIDE BUTTONS
+
+  RIGTH_PLUS_BUTTON.addEventListener("click", () => {
+    let value = parseFloat(RIGTH_INPUT.value) || 1.0;
+    value += 1;
+    RIGTH_INPUT.value = value.toFixed(2);
+    RIGTH_BET_BUTTON.textContent = `${value.toFixed(2)} BET`;
+  });
+  RIGTH_MINUS_BUTTON.addEventListener("click", () => {
+    let value = parseFloat(RIGTH_INPUT.value) || 1.0;
+    if (value > 1) {
+      value -= 1;
+      RIGTH_INPUT.value = value.toFixed(2);
+      RIGTH_BET_BUTTON.textContent = `${value.toFixed(2)} BET`;
+    }
+  });
+};
+
+const betValue = () => {
+  // LEFT SIDE BUTTONS
+
+  LEFT_ONE_BUTTON.addEventListener("click", () => {
+    LEFT_INPUT.value = "1.00";
+    LEFT_BET_BUTTON.textContent = `1.00 BET`;
+  });
+  LEFT_FIVE_BUTTON.addEventListener("click", () => {
+    LEFT_INPUT.value = "5.00";
+    LEFT_BET_BUTTON.textContent = `5.00 BET`;
+  });
+  LEFT_TWENTYFIVE_BUTTON.addEventListener("click", () => {
+    LEFT_INPUT.value = "25.00";
+    LEFT_BET_BUTTON.textContent = `25.00 BET`;
+  });
+  LEFT_ONEHUNDRED_BUTTON.addEventListener("click", () => {
+    LEFT_INPUT.value = "100.00";
+    LEFT_BET_BUTTON.textContent = `100.00 BET`;
+  });
+
+  // RIGHT SIDE BUTTONS
+
+  RIGTH_ONE_BUTTON.addEventListener("click", () => {
+    RIGTH_INPUT.value = "1.00";
+    RIGTH_BET_BUTTON.textContent = `1.00 BET`;
+  });
+  RIGTH_FIVE_BUTTON.addEventListener("click", () => {
+    RIGTH_INPUT.value = "5.00";
+    RIGTH_BET_BUTTON.textContent = `5.00 BET`;
+  });
+  RIGTH_TWENTYFIVE_BUTTON.addEventListener("click", () => {
+    RIGTH_INPUT.value = "25.00";
+    RIGTH_BET_BUTTON.textContent = `25.00 BET`;
+  });
+  RIGTH_ONEHUNDRED_BUTTON.addEventListener("click", () => {
+    RIGTH_INPUT.value = "100.00";
+    RIGTH_BET_BUTTON.textContent = `100.00 BET`;
+  });
+};
+
+
+airplane();
+betBtn();
+betValue();
